aacdd2a3ad6247c9292cbb8e53e164c0ac79d9a4
xuliangliang <xuliangliang@joyme.sg>
2021-06-16 16:42:59 +0800

    fix nonce_str

--- src/utils/code.js
+++ src/utils/code.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-08-10 15:20:53
+ * @LastEditTime: 2021-06-16 16:42:34
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /node-sdk/src/utils/code.js
+ */
 const utils = require('./utils');
 //  获取随机 位数
 function getRandom(num) {
@@ -5,7 +13,7 @@ function getRandom(num) {
 }
 
 module.exports = {
-    nonce_str:  `${getRandom(8)}${utils.getNow()}${getRandom(8)}`,
+    nonce_str:  () => `${getRandom(8)}${utils.getNow()}${getRandom(8)}`,
     request_id: getRandom(9),
     im_nonce: getRandom(8),
 };
